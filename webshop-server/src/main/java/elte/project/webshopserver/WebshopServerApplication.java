e9b790e089deb634eafcf1c1395468e7f38a169d
Csaba Hegedűs <csaba.hegedus.bgy@gmail.com>
2018-10-27 12:13:41 +0200

    Added basic maven.

---
+++ webshop-server/src/main/java/elte/project/webshopserver/WebshopServerApplication.java
@@ -0,0 +1,12 @@
+package elte.project.webshopserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebshopServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebshopServerApplication.class, args);
+	}
+}
